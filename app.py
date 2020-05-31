b1bbfe67b02ccae4f097914f9789a1b06cd5dbfb
Hans Daduya <hansdaduya@yahoo.com>
2020-05-31 14:32:19 +0800

    simple ai bot

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+# socketio = SocketIO(app)
+
+# @app.route('/')
+# def sessions():
+#     return render_template('session.html')
+
+# def messageReceived(methods=['GET', 'POST']):
+#     print('message was received!!!')
+
+# @socketio.on('my event')
+# def handle_my_custom_event(json, methods=['GET', 'POST']):
+#     print('received my event: ' + str(json))
+#     socketio.emit('my response', json, callback=messageReceived)
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
+
+
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from wit import Wit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
+client = Wit("2XWTIKVOL6RTJLGCQQ7OXDG6YQVBCTMH")
+
 
 @app.route('/')
 def sessions():
@@ -14,8 +41,38 @@ def messageReceived(methods=['GET', 'POST']):
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+    # print('received my event: ' + str(json))
+    # socketio.emit('my response', json, callback=messageReceived)
+    msg = {"user_name": "", "message": "", "bot_name": "Steve", "bot_message": ""}
+    if json["data"] == "User Connected":
+        socketio.emit('my response', json, callback=messageReceived)
+    elif json["data"] == "message":
+        resp = client.message(json["message"])
+        if resp['intents'][0]['name'] == "greeting":
+            msg["user_name"] = json["user_name"]
+            msg["message"] = json["message"]
+            msg["bot_message"] = "Hello there Human"
+        elif resp['intents'][0]['name'] == "temperature":
+            msg["user_name"] = json["user_name"]
+            msg["message"] = json["message"]
+            msg["bot_message"] = "I don't know but it's hot"
+        socketio.emit('my response', msg, callback=messageReceived)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
+
+
+
+
+# from flask import Flask, render_template
+# from flask_socketio import SocketIO
+# from wit import Wit
+
+# app = Flask(__name__)
+# client = Wit("2XWTIKVOL6RTJLGCQQ7OXDG6YQVBCTMH")
+
+# @app.route('/')
+# def sessions():
+#     resp = client.message('Hello')
+#     print('Yay, got Wit.ai response: ' + str(resp))
+#     return "hello, world"
