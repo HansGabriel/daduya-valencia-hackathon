b16003764821b7c665116adb7f193e4896a3ffda
Hans Daduya <hansdaduya@yahoo.com>
2020-06-04 22:02:12 +0800

    simple flask 3

--- app.py
+++ app.py
@@ -106,4 +106,4 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, cors_allowed_origins='*')
