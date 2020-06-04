8c74e0d32130b9761060496c1e41632621119e11
Hans Daduya <hansdaduya@yahoo.com>
2020-06-04 22:12:48 +0800

    simple flask 4

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def sessions():
@@ -106,4 +106,4 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, cors_allowed_origins='*')
+    socketio.run(app, debug=True)
